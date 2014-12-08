902dd5be5e3fb0f9e5b2a84a261145c3bc08fa5a
andersc <anderscui@gmail.com>
2014-12-08 22:11:01 +0800

    ML ex06

---
+++ ml/ml007/mlclass-ex6-007/test_loop.m
@@ -0,0 +1,10 @@
+function plotData(X, y)
+
+C_steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+Sigma_steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+for i = 1:length(C_steps)
+    disp(i);
+end
+
+end
